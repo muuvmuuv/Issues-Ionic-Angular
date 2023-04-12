49ba096f77937263aed97aaa572e824a3f222782
Marvin Heilemann <11534760+muuvmuuv@users.noreply.github.com>
2023-04-12 08:29:53 +0200

    Prettier

--- src/app/tabs.component.ts
+++ src/app/tabs.component.ts
@@ -1,26 +1,26 @@
-import { Component, EnvironmentInjector, inject } from "@angular/core";
-import { RouterModule } from "@angular/router";
-import { IonicModule } from "@ionic/angular";
+import { Component, EnvironmentInjector, inject } from "@angular/core"
+import { RouterModule } from "@angular/router"
+import { IonicModule } from "@ionic/angular"
 
 @Component({
-  selector: "app-tabs",
-  standalone: true,
-  imports: [IonicModule, RouterModule],
-  styles: [
-    `
-      :host {
-        display: contents;
-      }
-    `,
-  ],
-  template: `
-    <ion-tabs [environmentInjector]="environmentInjector">
-      <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="issue-27166"> 27166 </ion-tab-button>
-      </ion-tab-bar>
-    </ion-tabs>
-  `,
+	selector: "app-tabs",
+	standalone: true,
+	imports: [IonicModule, RouterModule],
+	styles: [
+		`
+			:host {
+				display: contents;
+			}
+		`,
+	],
+	template: `
+		<ion-tabs [environmentInjector]="environmentInjector">
+			<ion-tab-bar slot="bottom">
+				<ion-tab-button tab="issue-27166"> 27166 </ion-tab-button>
+			</ion-tab-bar>
+		</ion-tabs>
+	`,
 })
 export class TabsComponent {
-  environmentInjector = inject(EnvironmentInjector);
+	environmentInjector = inject(EnvironmentInjector)
 }
